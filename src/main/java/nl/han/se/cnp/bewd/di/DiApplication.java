04a756f5270d58387e59b2478ad30108fe4db596
Meron Brouwer <meron.brouwer@gmail.com>
2024-08-29 09:31:52 +0200

    Initial commit

---
+++ src/main/java/nl/han/se/cnp/bewd/di/DiApplication.java
@@ -0,0 +1,13 @@
+package nl.han.se.cnp.bewd.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiApplication.class, args);
+	}
+
+}
