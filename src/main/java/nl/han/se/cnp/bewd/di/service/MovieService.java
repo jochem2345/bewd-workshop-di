86de3f54e5b6e5d75bc5ea7e84295d63e585f31e
Jochem Jansen <jd.jansen2@student.han.nl>
2024-09-23 19:48:15 +0200

    Beetje gespeeld met value

--- src/main/java/nl/han/se/cnp/bewd/di/service/MovieService.java
+++ src/main/java/nl/han/se/cnp/bewd/di/service/MovieService.java
@@ -4,6 +4,7 @@ import nl.han.se.cnp.bewd.di.domain.Movie;
 import nl.han.se.cnp.bewd.di.repository.MovieList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
